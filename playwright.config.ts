e1672aa9be454509d140cee44bde7df42dd89840
Sam Neubardt <sam@upstream.tech>
2026-03-17 09:54:43 -0400

    Switch dev server port to 4000

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: "list",
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:4000",
     trace: "on-first-retry",
   },
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:3000",
+    url: "http://localhost:4000",
     reuseExistingServer: !process.env.CI,
     timeout: 30000,
   },
